365554607ed6565808e63635e24b0a1b296367e2
hd-code <54773293+hd-code@users.noreply.github.com>
2022-06-21 13:43:55 +0200

    flat structure for paper

--- skill/paper/make.sh
+++ skill/paper/make.sh
@@ -4,10 +4,8 @@ pandock() {
     docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) "$@"
 }
 
-cd assets
 pandock pandoc-skill --biblatex content.md -o content.tex
 
-cd ..
 pandock --entrypoint pdflatex pandoc-skill paper
 pandock --entrypoint biber pandoc-skill paper
 pandock --entrypoint pdflatex pandoc-skill paper
